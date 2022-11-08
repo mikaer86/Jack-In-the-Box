c60d7713184d720b4f4aab51916f55f737d105a1
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-08 19:11:16 +0100

    check if this is ok

---
+++ js/game.js
@@ -0,0 +1,21 @@
+
+function renderGame() {
+
+    const gameWindow = document.querySelector("#game-container");
+    const gameBoard= document.querySelector("gameBoard");
+    const gameZones = document.querySelector("gameBoard- zones");
+    const loadWindow = window.onload;
+
+    
+
+    loadWindow.addEventlistener('click', function(){
+
+
+
+    });
+
+
+    
+
+
+}
