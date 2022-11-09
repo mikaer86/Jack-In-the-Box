3abe11862fb1ae70ae7126ebaa7e5d9b758b9fdf
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-09 22:43:52 +0100

    renderGame function

--- js/game.js
+++ js/game.js
@@ -1,21 +1,46 @@
+window.onload = renderGame();
 
 function renderGame() {
 
-    const gameWindow = document.querySelector("#game-container");
-    const gameBoard= document.querySelector("gameBoard");
-    const gameZones = document.querySelector("gameBoard- zones");
-    const loadWindow = window.onload;
+    const targetGameWindowElement = document.querySelector("#game-container");
+    const userInput = document.querySelector("cellAmount");
 
+    const createUserInput = document.createElement("input");
+    const createGameBoardElement = document.createElement("section");
+    const createGameZoneUl = document.createElement("ul");
+    const createGameZoneLi = document.createElement("li");
+
+    createUserInput.className = "cellAmount";
+    createUserInput.type = "number";
+    createUserInput.value = "value";
+    createGameBoardElement.className ="gameBoard";
+    createGameZoneUl.className = "gameBoard-zones";
+    createGameZoneLi.className = "zone";
+
+    targetGameWindowElement.append(createUserInput);
+    targetGameWindowElement.append(createGameBoardElement);
+    createGameBoardElement.append(createGameZoneUl);
+    createGameZoneUl.append( createGameZoneLi);
+
+  
+    // inputCellAmount();
     
-
-    loadWindow.addEventlistener('click', function(){
-
-
-
-    });
-
-
-    
-
 
 }
+
+// function inputCellAmount (userInput) {
+
+//     if(!userInput.value === null || -1){
+    
+//         for(let i = userInput.value; i > 0; i++ ) {
+
+
+//         }
+
+//     } else {
+
+//         console.log("Input a none negative value");
+//     }
+
+//     return ;
+// }
