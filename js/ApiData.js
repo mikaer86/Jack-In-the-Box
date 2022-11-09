35b7d61ddd1fec7f15093fd5113ae08c469add2f
setare <setarehsaboory@gmail.com>
2022-11-10 00:39:27 +0100

    fetch is working !

--- js/ApiData.js
+++ js/ApiData.js
@@ -1,21 +1,15 @@
 'use strict'
-const playBtn = document.querySelector('#play-btn');
-const div = document.querySelector('#div');
-
-
-//👇 when ever user clicks PLAY button , Board renders
-playBtn.addEventListener('click', async function () {
-    const allPokesData = await fetchAllPokes();
-    initializeBoard(allPokesData);
-})
+// const playBtn = document.querySelector('#play-btn');
+// const zones = document.querySelectorAll('.zone');
+export let pokeMons = [];
 //👇 Fetching all 150 datas from Pokemon Api and we get 150 datas in return, limit=150 is an endpoint and changeable
-async function fetchAllPokes() {
+export async function fetchAllPokes() {
     const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150');
     const data = await res.json();
-    return data;
+    initializeBoard(data);
+    return pokeMons;
 }
 function initializeBoard(allPokesData) {
-    div.innerHTML = '';
     //👇in .results we have a array of all pokemons --> each pokemon is array of 2 keys (name & url)
     const pokemonsArr = allPokesData.results;
     //👇for getting random pokemons we make random numbers between 0 to 150 -->we need to fetch url of each random pokemon to get each pokemon's info individually
@@ -26,19 +20,16 @@ function initializeBoard(allPokesData) {
 }
 
 
-function fetchPokeData(pokemon) {
+async function fetchPokeData(pokemon) {
     const url = pokemon.url;
-    fetch(url)
-        .then(response => response.json())
-        .then(function (pokeData) {
-            renderPoke(pokeData);
-        })
+    const res = await fetch(url);
+    const data = await res.json();
+    pokeMons.push(data);
+}
 
-}
-function renderPoke(pokeData) {
-    let pokeImage = document.createElement('img');
-    //👇 in order to get image of a pokemon --> we need pokemon id (which is integers like 1,2,3).
-    pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png`;
-    div.append(pokeImage);
-    pokeImage.after(pokeData.name);
-}
+
+// let pokeImage = document.createElement('img');
+// 👇 in order to get image of a pokemon --> we need pokemon id (which is integers like 1,2,3).
+// pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeData.id}.png`;
+// div.append(pokeImage);
+// pokeImage.after(pokeData.name);
--- js/game.js
+++ js/game.js
@@ -1,21 +1,30 @@
+import { fetchAllPokes, pokeMons } from './ApiData.js';
+//👇just call this function when ever we need a new randomed pokemond (ex:to play again)
+const data = fetchAllPokes();
+//🧷it takes time to fetch (it's normal);
+setTimeout(() => {
+    console.log(pokeMons[1].id);
+    let pokeImage = document.createElement('img');
+    pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeMons[1].id}.png`;
+    document.body.append(pokeImage);
+}, 4000);
+
 
 function renderGame() {
 
     const gameWindow = document.querySelector("#game-container");
-    const gameBoard= document.querySelector("gameBoard");
+    const gameBoard = document.querySelector("gameBoard");
     const gameZones = document.querySelector("gameBoard- zones");
     const loadWindow = window.onload;
 
-    
-
-    loadWindow.addEventlistener('click', function(){
 
 
-
+    loadWindow.addEventlistener('click', function () {
+        //clean board each time window is loaded ??? // zones.innerHTML = '' ??
     });
 
 
-    
+
 
 
 }
