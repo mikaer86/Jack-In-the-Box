adc8e6b322fd839ea1a205665fb176b6393e4a7c
setare <setarehsaboory@gmail.com>
2022-11-10 14:00:30 +0100

    images taken

--- js/game.js
+++ js/game.js
@@ -1,26 +1,33 @@
 import { fetchAllPokes, pokeMons } from './ApiData.js';
 //👇just call this function when ever we need a new randomed pokemond (ex:to play again)
-const data = fetchAllPokes();
 //🧷it takes time to fetch (it's normal);
+
+let pok = fetchAllPokes();
+const zones = document.querySelectorAll(".zone");
 setTimeout(() => {
-    console.log(pokeMons[1].id);
-    let pokeImage = document.createElement('img');
-    pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeMons[1].id}.png`;
-    document.body.append(pokeImage);
-}, 4000);
+    for (let i = 0; i < pokeMons.length; i++) {
+        let pokeImage = document.createElement('img');
+        pokeImage.srcset = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokeMons[i].id}.png`;
+        zones[i].append(pokeImage);
+    }
+
+}, 3000);
+
+
 
 
 function renderGame() {
 
     const gameWindow = document.querySelector("#game-container");
     const gameBoard = document.querySelector("gameBoard");
-    const gameZones = document.querySelector("gameBoard- zones");
+    const gameZones = document.querySelector("gameBoard-zones");
     const loadWindow = window.onload;
 
 
 
     loadWindow.addEventlistener('click', function () {
-        //clean board each time window is loaded ??? // zones.innerHTML = '' ??
+        // do we need to clean board each time window is loaded ??? // zones.innerHTML = '' ??
+
     });
 
 
