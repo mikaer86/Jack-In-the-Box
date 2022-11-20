e0604cf0feae0f661cb00ec7fe58248a3733cb89
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-20 21:57:36 +0100

    import timer and showTimer

--- js/game.js
+++ js/game.js
@@ -1,15 +1,21 @@
 import { fetchAllPokes, pokeMons } from './ApiData.js';
 import { initializeBoard, circles } from '../js/Board.js';
 import { validateGame } from '../js/validation.js';
+import { timer, showTimer } from '../js/timer.js';
 fetchAllPokes();
 
 const playBtn = document.querySelector('.playBtn');
 let playingStatus = false;
+let startTime = false;
 
 playBtn.addEventListener('click', function () {
     // shuffle(pokeMons);
     playingStatus = initializeBoard(pokeMons);
+    startTime = showTimer();
+
+
     // startTiming()+ displaying; // the way harry said
+
 
 })
 
