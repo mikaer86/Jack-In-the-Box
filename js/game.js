9da6d7381d299df7c906de4d121c6d67d2e262ab
setare <setarehsaboory@gmail.com>
2022-11-22 12:49:25 +0100

    shuffled pokemones

--- js/game.js
+++ js/game.js
@@ -1,43 +1,27 @@
 import { fetchAllPokes, pokeMons } from './ApiData.js';
 import { initializeBoard, circles } from '../js/Board.js';
 import { validateGame } from '../js/validation.js';
-import { timer, showTimer } from '../js/timer.js';
-fetchAllPokes();
-
+import { timer } from '../js/timer.js';
 const playBtn = document.querySelector('.playBtn');
 let playingStatus = false;
 let startTime = false;
 
+fetchAllPokes();
 playBtn.addEventListener('click', function () {
-    // shuffle(pokeMons);
-<<<<<<< HEAD
-    playingStatus = initializeBoard(pokeMons); //full=0
-    // startTiming() + displaying; // the way harry said
-    // timer = setInterval(() => {
-    //     timeElement = 15--;
-    //     if(time){
-    //         check(full); 12?or not
-    //     }
-    //     if (timer != true) {
-    //         playingStatus = false
-    //     }
-    // }, 1000);
-
-=======
     playingStatus = initializeBoard(pokeMons);
-    startTime = showTimer();
+    console.log(playingStatus);
+    // startTime = showTimer();
 
 
     // startTiming()+ displaying; // the way harry said
->>>>>>> e0604cf0feae0f661cb00ec7fe58248a3733cb89
 
 
 })
 
 
-for (let i = 0; i < 12; i++) {
-    circles[i].addEventListener("click", function () {
-        validateGame(playingStatus, circles[i]);
+// for (let i = 0; i < 12; i++) {
+//     circles[i].addEventListener("click", function () {
+//         validateGame(playingStatus, circles[i]);
 
-    });
-}
+//     });
+// }
--- js/timer.js
+++ js/timer.js
@@ -1,19 +1,15 @@
-
-function timer() {
-
+'use strict'
+export let timeDuration;
+export function timer() {
     let currentTime = date.now();
-    let timeDuration = timeDuration - (date.now() - currentTime / 1000) | 0;
+    timeDuration = timeDuration - (date.now() - currentTime / 1000) | 0;
     seconds;
-
-
     let seconds = timeDuration % 60 | 0;
     let condSeconds = seconds < 10 ? "0" + seconds : seconds;
-
-
     setInterval(function () {
 
 
-
+        console.log("timing");
 
     }, 1000);
 
