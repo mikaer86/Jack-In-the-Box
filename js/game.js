3b5fa80565e70053dd35459e6955bab488ab35ef
mikaer86 <mikaeleriksson123@gmail.com>
2022-11-22 23:33:18 +0100

    imported startGameTimer, stopGameTimer

--- js/game.js
+++ js/game.js
@@ -1,28 +1,44 @@
 import { fetchAllPokes, pokeMons } from './ApiData.js';
 import { initializeBoard, circles } from '../js/Board.js';
 import { validateGame } from '../js/validation.js';
-import { timer, showTimer } from '../js/timer.js';
+import { startGameTimer, stopGameTimer, timeDuration } from '../js/timer.js';
 fetchAllPokes();
 
 const playBtn = document.querySelector('.playBtn');
 let playingStatus = false;
 let startTime = false;
+let stopTime = false;
 
 playBtn.addEventListener('click', function () {
     // shuffle(pokeMons);
     playingStatus = initializeBoard(pokeMons);
-    startTime = showTimer();
+
+    if (playingStatus) {
+
+        startTime = startGameTimer();
+
+        while (startTime) {
+
+            if (timeDuration <= 0) {
+
+                stopTime = stopGameTimer();
+
+            }
 
 
-    // startTiming()+ displaying; // the way harry said
+        }
 
 
-})
+        // startTiming()+ displaying; // the way harry said
+
+
+    });
 
 
 for (let i = 0; i < 12; i++) {
     circles[i].addEventListener("click", function () {
         validateGame(playingStatus, circles[i]);
+
 
     });
 }
